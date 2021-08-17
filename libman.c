5a7d6c32d9dab9e9d1d9c67ad77ccc40d0af7d05
AMAL <mailtoamaldevcd@gmail.com>
2021-08-17 21:06:19 +0530

    Struct

--- libman.c
+++ libman.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
-int main()
+struct user
 {
+	int uid;
+	char name[50];
+	char address[100];
+	int cellno;
+};
+struct book
+{
+	int bid;
+	char name[50];
+	char author[50];
+	int price;
+
+};
+int main()
+{	
 	printf("library");
 	return 0;
 }
