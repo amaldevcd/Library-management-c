45713c74da4a05b5563e079422fb189fd46c03c8
tomgeorge_tgp <tomgeorgetgp@gmail.com>
2021-08-17 19:34:24 +0530

    rules

--- libman.c
+++ libman.c
@@ -1,6 +1,111 @@
 #include <stdio.h>
-int main()
+void rules();
+void main()
 {
-	printf("library");
-	return 0;
+	printf("LIBRARY \n");
+
+	printf("  \n  ");
+	printf("WELCOME\n");
+	printf("\n");
+	 //array to store the name of the Members of libary
+	String member[25];    
+	    member[0]=
+        member[1]=
+        member[2]=
+        member[0]=	     
+        member[3]=
+        member[4]=
+        member[5]=
+        member[6]=
+        member[7]=	 
+        member[8]=
+		//array to store the name of the Books of libary
+	String book[25];     
+	    book[0] ="In Search of Lost Time by Marcel Proust";
+        book[1] ="Ulysses by James Joyce";
+        book[2] ="Don Quixote by Miguel de Cervantes"; 
+        book[3] ="Moby Dick by Herman Melville";
+        book[4] ="Hamlet by William Shakespeare";
+        book[5] ="War and Peace by Leo Tolstoy";
+        book[6] ="The Odyssey by Homer";
+        book[7] ="The Great Gatsby by F.Scott Fitzgerald";
+        book[8] ="The Divine Comedy by Dante Alighieri";
+        book[9] ="The Adventure of Huckleberry Finn by Mark Twain";
+        book[10] ="Lolita by Vladimir Nabokov";
+        book[11] ="Anna Karenina by Leo Tolstoy";
+        book[12] ="Pride and Prejudice by Jane Austen";
+        book[13] ="Wuthering Heights by Emily Bronte";
+        book[14] ="Great Expectations by Charles Dickens";
+        book[15] ="David Copperfield by Charles Dickens";
+        book[16] ="The Canterbury Tales by Geoffery Chaucer";
+        book[17] ="Les Miserables by Victor Hugo";
+        book[18] ="To Kill a Mocking bird by Harper Lee";
+        book[19] ="Frankenstein by Mary Shelley";
+         
+	  switch (choice)
+	  {
+	  case 1:
+		  break;
+	  case 2:
+	      break;
+	  case 3:
+	      break;
+	  case 4:
+	      break;
+	  case 5:
+	      rules();
+	      break;	  	  	  
+	  default:
+		  break;
+	  }
+}
+
+void rules()
+{
+	printf(" LIBARY RULES AND REGULATIONS  \n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("  OPENING HOURS  \n");
+	printf("\n");
+	printf("\n");
+	printf(" Monday - Friday  08:00AM to 10:00PM\n");
+	printf(" Saturday         09:00AM to 06:00PM\n");
+    printf(" The libary is closed on Sundays and all public holidays\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("   DISCIPLINE  \n");
+	printf("\n");
+	printf("\n");
+	printf(" All users must observe total silence in the library and its enviroment at all times");
+	printf(" All users are required to show their Library IDs to the security officer upon entrance to the library");
+	printf(" Use of mobiles phones is strictly prohibited in the library\n");
+	printf(" All bags,cases,folders etc. must be left in the luggage area outside the library.\n");
+	printf(" Users are not allowed to leave their baggage overnight in the library. The library shall not take any responsibility for loss of personal property or book already signed out to a user.\n");
+	printf("\n");
+	printf("\n");
+	printf("    DAMAGE/LOSS OF LIBRARY MATERIALS   \n");
+	printf("\n");
+	printf("\n");
+	printf(" All users will be held responsible for any damage or loss of library materials in their possession and wll be required to meet the cost of replacement and processing\n");
+	printf(" Users must ensure that the books they borrow are in good condition to avoid being held responsible for any damage noted while returning the books\n");
+	printf(" Lost books must be reported to the librarian immediately and replaced or paid for within 30 days\n");
+	printf(" Lost library books that are recovered, must be handed to the librarian as they remain the property of college library\n");
+	printf("\n");
+	printf("\n");
+	printf("  OVERDUE CHARGES/FINES  \n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf(" UPTO 5 DAYS      Re 1 PER DAY\n");
+	printf(" NEXT 5 DAYS      Rs 2 PER DAY\n");
+	printf(" NEXT 10 DAYS     Rs 4 PER DAY\n");
+	printf(" ABOVE THAT       Rs 5 PER DAY\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("     THANK YOU    \n");
+	
 }
